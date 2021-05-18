e3e9987610b0d2fd3b32bf891f1ce32e84791029
Garrett Fite <36345799+Stranded-Helix@users.noreply.github.com>
2021-05-17 19:14:10 -0500

    fixing merge issues

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -1,5 +1,53 @@
 const router = require('express').Router();
-//const { Product } = require('../../models'); FIXME: Uncomment
+const { Product } = require('../../models');
+
+//gets all products from db
+router.get('/', async (req, res) => {
+    Product.findAll()
+    .then((products) => {
+        res.json(products);
+    })
+})
+//gets product by id
+router.get('/:id', async (req, res) => {
+    Product.findByPk(req.params.id)
+    .then((singleProduct) => {
+        res.json(singleProduct);
+    })
+})
+
+//const res = await axios.put('https://localhost:3001/api/product/', { hello: 'world' });
+
+const updateProductOwner = (user, items) => {
+    items.map((x) => {
+        {owner: user},
+        {where: {product_id: x }}
+    })
+}
+
+//changes owner of product
+router.put('/:userid/:product/', async (req, res) => {
+    //Check product ids where order number in order items match
+    //update the ownership of all items in order to user_id
+    console.log(req);
+    Product.update(
+        {
+        owner: 1//FIXME: hardcoded value
+        },
+        {
+            where: {
+                product_id: 2//FIXME: hardcoded value
+            }
+        }
+    )
+    .then((updatedProduct) => {
+        res.json(updatedProduct)
+    })
+    .catch((err) => {
+        console.log(err);
+        res.json(err);
+    })
+})
 
 router.post('/', async (req, res) => {
     //TODO: Handle adding item to order, check login status before update
